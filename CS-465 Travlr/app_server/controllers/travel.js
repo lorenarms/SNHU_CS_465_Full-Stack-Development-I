e35a065b08d7bba40ee049a91f8062382773a360
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2022-09-03 08:13:50 -0700

    added static json to travel page

--- CS-465 Travlr/app_server/controllers/travel.js
+++ CS-465 Travlr/app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+const fs = require('fs')
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
+
+// GET Travel View
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    pageTitle = 'Travlr Getaways - Travel';
+    res.render('travel', {title: pageTitle, trips});
 };
 
 module.exports = {

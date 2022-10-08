3dc13527b11bcf67c256abbc11f63e46bac94243
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2022-10-08 15:22:09 -0700

    implemented register and log-in for backend

--- CS-465 Travlr/app_api/models/db.js
+++ CS-465 Travlr/app_api/models/db.js
@@ -41,4 +41,4 @@ process.on('SIGTERM', () => {
   });
 });
 
-require('./travlr');
+require('./trips');
--- CS-465 Travlr/app_api/routes/index.js
+++ CS-465 Travlr/app_api/routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const authController = require('../controllers/authentication')
 const tripsController = require('../controllers/trips');
+
+router
+    .route('/login')
+    .post(authController.login);
+
+router
+    .route('/register')
+    .post(authController.register);
 
 router
     .route('/trips')
